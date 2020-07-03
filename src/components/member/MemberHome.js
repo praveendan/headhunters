843ae51917774f9246ad61da8748684eaea9ec5b
praveen dhanasekara <praveen@liveroom.xyz>
2020-07-03 11:58:06 +0530

    refactored member home to functional

--- src/components/member/MemberHome.js
+++ src/components/member/MemberHome.js
@@ -1,69 +1,62 @@
 import React from 'react';
 import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 
-import TopBar from '../shared/TopBar';
 import {Colors} from '../shared/ColourSheet';
 import Icon from 'react-native-vector-icons/Foundation';
 import CommonStyles from '../shared/Common.style';
 import {MemberItemType} from '../shared/Strings';
 
-export default class MemberHome extends React.Component {
-  state = {userKey: '', errorMessage: null};
-  openEvents = () => {
-    this.props.navigation.navigate('MemberUpdatesList', {
+export default function MemberHome({route, navigation}) {
+  var openEvents = () => {
+    navigation.navigate('MemberUpdatesList', {
       subType: MemberItemType.EVENTS,
     });
   };
 
-  openNews = () => {
-    this.props.navigation.navigate('MemberUpdatesList', {
+  var openNews = () => {
+    navigation.navigate('MemberUpdatesList', {
       subType: MemberItemType.NEWS,
     });
   };
-  render() {
-    return (
-      <View style={CommonStyles.container}>
-        <View style={CommonStyles.viewContainer}>
-          <TouchableOpacity
-            style={styles.viewContainerItem}
-            onPress={this.openEvents}>
-            <View style={styles.viewContainerItemMain}>
-              <Text style={styles.viewContainerItemHeading}>EVENTS</Text>
-              <Text style={styles.viewContainerItemSubHeading}>
-                UPCOMING EVENTS
-              </Text>
-            </View>
-            <Text style={styles.viewContainerItemViewMoreItems}>
-              VIEW ALL EVENTS
+
+  return (
+    <View style={CommonStyles.container}>
+      <View style={CommonStyles.viewContainer}>
+        <TouchableOpacity style={styles.viewContainerItem} onPress={openEvents}>
+          <View style={styles.viewContainerItemMain}>
+            <Text style={styles.viewContainerItemHeading}>EVENTS</Text>
+            <Text style={styles.viewContainerItemSubHeading}>
+              UPCOMING EVENTS
             </Text>
-            <Icon
-              style={styles.viewContainerItemViewMoreItemsArrow}
-              name="arrow-right"
-              color={Colors.light}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.viewContainerItem}
-            onPress={this.openNews}>
-            <View style={styles.viewContainerItemMain}>
-              <Text style={styles.viewContainerItemHeading}>NEWS</Text>
-              <Text style={styles.viewContainerItemSubHeading}>
-                INTERCOM NEWS AND UPDATES
-              </Text>
-            </View>
-            <Text style={styles.viewContainerItemViewMoreItems}>
-              VIEW NEWS UPDATES
+          </View>
+          <Text style={styles.viewContainerItemViewMoreItems}>
+            VIEW ALL EVENTS
+          </Text>
+          <Icon
+            style={styles.viewContainerItemViewMoreItemsArrow}
+            name="arrow-right"
+            color={Colors.light}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.viewContainerItem} onPress={openNews}>
+          <View style={styles.viewContainerItemMain}>
+            <Text style={styles.viewContainerItemHeading}>NEWS</Text>
+            <Text style={styles.viewContainerItemSubHeading}>
+              INTERCOM NEWS AND UPDATES
             </Text>
-            <Icon
-              style={styles.viewContainerItemViewMoreItemsArrow}
-              name="arrow-right"
-              color={Colors.light}
-            />
-          </TouchableOpacity>
-        </View>
+          </View>
+          <Text style={styles.viewContainerItemViewMoreItems}>
+            VIEW NEWS UPDATES
+          </Text>
+          <Icon
+            style={styles.viewContainerItemViewMoreItemsArrow}
+            name="arrow-right"
+            color={Colors.light}
+          />
+        </TouchableOpacity>
       </View>
-    );
-  }
+    </View>
+  );
 }
 const styles = StyleSheet.create({
   viewContainerItem: {
